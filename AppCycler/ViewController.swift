8c89e016eeff5170e7ded33939a8d7a9831ded5b
tbrek <me@tombrek.com>
2018-04-04 13:51:20 +0100

    Initial commit

---
+++ AppCycler/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AppCycler
+//
+//  Created by Tom Brek on 04/04/2018.
+//  Copyright © 2018 Tom Brek. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
